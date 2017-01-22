316d99deb5c53f3a5c01907f44b4dbd65fdc04fd
Andrea Baty <Andrea.f.baty@gmail.com>
2017-01-22 17:56:20 -0500

    gradient needs work

--- main.js
+++ main.js
@@ -45,14 +45,44 @@ window.setInterval(currentTime, 1000);
 function backColor(){
   var time = new Date();
   var s = time.getSeconds();
-  var color = '#'+'0D'+'4F'+s;
+  var hex3 = s + 15;
+  var color = '#'+ '5F' + '5D' + hex3;
   document.body.style.background = color;
+
+// HOVER following code found in link provided in assignement: http://eloquentjavascript.net/14_event.html#h_HQoLxG2r2l
+  var hover = document.querySelector('h1');
+  function isInside (node, target){
+    for (; node != null; node = node.parentNode)
+      if (node == target) return true;
+  }
+  hover.addEventListener ("mouseover", function (event){
+    if (!isInside(event.relatedTarget, hover))
+    hours.textContent = '5F';
+    minutes.textContent = '5D';
+    seconds.textContent = hex3;
+  });
+  hover.addEventListener('mouseout', function(event){
+    if (!isInside(event.relatedTarget, hover))
+    hours.textContent = hr;
+    minutes.textContent = min;
+    seconds.textContent = sec;
+  });
 }
 
 window.setInterval(backColor, 1000);
 
+// radial gradient //
+// http://stackoverflow.com/questions/442404/retrieve-the-position-x-y-of-an-html-element //
+var rect = document.getElementById("timeBox").getBoundingClientRect();
+console.log(rect.top, rect.right, rect.bottom, rect.left);
 
+// radial gradient code from https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/createRadialGradient //
+var canvas = document.getElementById('wrapper');
+var ctx = canvas.getContext('2d');
 
+var gradient = ctx.createRadialGradient(500, 350, 100, 500, 350, 200);
+ctx.fillStyle = gradient;
+ctx.fillRect(0,0,200,200);
 
 
 }());
